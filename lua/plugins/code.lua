d7b2c5fc8261716a8265aea0f451dfd415ee6cc7
Robin <robin.tordly@nav.no>
2024-05-23 21:20:44 +0200

    update

---
+++ lua/plugins/code.lua
@@ -0,0 +1,36 @@
+return {
+
+    -----------------------------------------------------------------------------
+    -- Powerful line and block-wise commenting
+    {
+        'numToStr/Comment.nvim',
+        event = 'VeryLazy',
+        dependencies = { 'JoosepAlviste/nvim-ts-context-commentstring' },
+        keys = {
+            { '<Leader>v', '<Plug>(comment_toggle_linewise_current)',  mode = 'n', desc = 'Comment line' },
+            { '<Leader>v', '<Plug>(comment_toggle_linewise_visual)',   mode = 'x', desc = 'Comment line' },
+            { '<Leader>V', '<Plug>(comment_toggle_blockwise_current)', mode = 'n', desc = 'Comment block' },
+            { '<Leader>V', '<Plug>(comment_toggle_blockwise_visual)',  mode = 'x', desc = 'Comment block' },
+        },
+        opts = function(_, opts)
+            local ok, tcc =
+                pcall(require, 'ts_context_commentstring.integrations.comment_nvim')
+            if ok then
+                opts.pre_hook = tcc.create_pre_hook()
+            end
+        end,
+    },
+
+	-----------------------------------------------------------------------------
+	-- Perform diffs on blocks of code
+	{
+		'AndrewRadev/linediff.vim',
+		cmd = { 'Linediff', 'LinediffAdd' },
+		keys = {
+			{ '<Leader>mdf', ':Linediff<CR>', mode = 'x', desc = 'Line diff' },
+			{ '<Leader>mda', ':LinediffAdd<CR>', mode = 'x', desc = 'Line diff add' },
+			{ '<Leader>mds', '<cmd>LinediffShow<CR>', desc = 'Line diff show' },
+			{ '<Leader>mdr', '<cmd>LinediffReset<CR>', desc = 'Line diff reset' },
+		},
+	},
+}
