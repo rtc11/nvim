05a462c17af2887a17df3a170161091a667707ab
robin <robin.tordly@gmail.com>
2026-03-17 09:10:53 +0100

    unzip jar files to allow gd and gI

--- lsp/kotlin_lsp.lua
+++ lsp/kotlin_lsp.lua
@@ -1,3 +1,4 @@
+-- update in :Mason
 return {
     filetypes = { 'kotlin' },
     cmd = { 'kotlin-lsp', '--stdio' },
@@ -15,5 +16,40 @@ return {
             }
         }
     },
+    on_attach = function(_, _)
+        if vim.g._jar_uri_handler then return end
+        vim.g._jar_uri_handler = true
+
+        vim.api.nvim_create_autocmd("BufReadCmd", {
+            pattern = "jar://*",
+            callback = function(ev)
+                local jar_path, internal_path = ev.match:match("^jar://(.-)!/(.+)$")
+                if not jar_path or not internal_path then
+                    vim.notify("Could not parse jar URI: " .. ev.match, vim.log.levels.ERROR)
+                    return
+                end
+
+                local result = vim.system({ "unzip", "-p", jar_path, internal_path }):wait()
+                if result.code ~= 0 then
+                    vim.notify("Failed to extract from JAR: " .. (result.stderr or ""), vim.log.levels.ERROR)
+                    return
+                end
+
+                local lines = vim.split(result.stdout, "\n", { trimempty = false })
+                vim.api.nvim_buf_set_lines(ev.buf, 0, -1, false, lines)
+
+                vim.bo[ev.buf].buftype = "nofile"
+                vim.bo[ev.buf].modifiable = false
+                vim.bo[ev.buf].readonly = true
+                vim.bo[ev.buf].swapfile = false
+
+                local ext = internal_path:match("%.(%w+)$")
+                if ext then
+                    local ft_map = { kt = "kotlin", java = "java", xml = "xml" }
+                    vim.bo[ev.buf].filetype = ft_map[ext] or ext
+                end
+            end,
+        })
+    end,
 }
 
